d025f0c9a7211403677c31be800b592e590ddc78
Kaiiorg <k349@live.com>
2020-09-06 13:10:13 -0500

    Basic wifi web server to read from sd card, but fails for some reason on larger files

---
+++ WiFiSDWebServer.ino
@@ -0,0 +1,156 @@
+/*
+  WiFi SD Web Server
+
+  A web server that accepts an HTTP request and responds with a picture of a cat
+
+  You will probably need to change SD_SELECT_PIN to whatever your hardware uses
+
+  created 13 July 2010 as WiFi Web Server
+  by dlf (Metodo2 srl)
+  modified 31 May 2012
+  by Tom Igoe
+  modified 6 September 2020 to read from SD card
+  by Kolton B
+ */
+
+#include <SPI.h>
+#include <WiFiNINA.h>
+
+#define SD_SELECT_PIN A0
+#define SD_BUFFER_SIZE 1024
+#include <SdFat.h>
+
+
+#include "arduino_secrets.h" 
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                 // your network key Index number (needed only for WEP)
+SdFat SD;
+
+int status = WL_IDLE_STATUS;
+
+WiFiServer server(80);
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // init the sd card
+  if (!SD.begin(SD_SELECT_PIN)) {
+    Serial.println("SD failed to init");
+    while (true);
+  }
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  server.begin();
+  // you're connected now, so print out the status:
+  printWifiStatus();
+}
+
+
+void loop() {
+  // listen for incoming clients
+  WiFiClient client = server.available();
+  if (client) {
+    Serial.println("new client");
+    // an http request ends with a blank line
+    boolean currentLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        // if you've gotten to the end of the line (received a newline
+        // character) and the line is blank, the http request has ended,
+        // so you can send a reply
+        if (c == '\n' && currentLineIsBlank) {
+          
+          File file = SD.open("cat.jpg");
+          if (file) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: image/jpg");
+            client.println("Connection: close");  // the connection will be closed after completion of the response
+            client.println("Content-Length: " +  String(file.fileSize()));
+            client.println();
+            
+            int totalBytesRead = 0;
+            char buffer[SD_BUFFER_SIZE];
+            while (file.available()) {
+              if (!client.connected()) {
+                Serial.println("Client not connected!");
+              }
+              // Read up to SD_BUFFER_SIZE bytes from the file
+              int bytesRead = file.read(buffer, SD_BUFFER_SIZE);
+
+              totalBytesRead += bytesRead;
+              client.write(buffer, bytesRead);
+            }
+            Serial.println("Read " + String(totalBytesRead) + " from file");
+          } else {
+            client.println("HTTP/1.1 404 NOTFOUND");
+            client.println("Connection: close");  // the connection will be closed after completion of the response
+            client.println();
+          }
+          
+          break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } else if (c != '\r') {
+          // you've gotten a character on the current line
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+
+    // close the connection:
+    client.stop();
+    Serial.println("client disconnected");
+  }
+}
+
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
